e79e748080d7441cf0b86b1760790eb4768ac9da
zzf <ybmking@163.com>
2021-03-23 17:07:26 +0800

    前端-添加导航菜单

--- panku-manage-front/src/components/HomeCommon.vue
+++ panku-manage-front/src/components/HomeCommon.vue
@@ -8,9 +8,62 @@
       </div>
     </el-header>
     <el-container>
-      <el-aside width="300px">Aside</el-aside>
+      <el-aside width="300px">
+        <el-row class="300px">
+        <el-col :span="12">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+        </el-row>
+      </el-aside>
       <el-container>
-        <el-main>Main</el-main>
+        <el-main>
+          <div class="breadcrumb">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+        </el-main>
       </el-container>
     </el-container>
     <el-footer>Footer</el-footer>
@@ -30,19 +83,41 @@ export default {
     // this.userData = this.$route.params.userData
     this.userData = JSON.parse(localStorage.getItem("userInfo"))
   },
-
+  handleOpen(key, keyPath) {
+    console.log(key, keyPath);
+  },
+  handleClose(key, keyPath) {
+    console.log(key, keyPath);
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.el-header, .el-footer{
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 .el-aside {
   background-color: #d3dce6;
+}
+.el-aside .el-row{
+  height: 100%;
+}
+.el-aside .el-row .el-menu{
+  height: 100%;
 }
 body > .el-container {
   margin-bottom: 40px;
   height: 100%;
   width: 100%;
   position: absolute;
+}
+.el-col-12 {
+  width: 100%;
+  height: 100%;
+  text-align: left;
 }
 </style>
