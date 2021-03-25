9fbed424f2ab9672634af2c7ab6d1317040525ba
zzf <ybmking@163.com>
2021-03-25 18:14:00 +0800

    后端-渲染导航栏

--- panku-manage-front/src/components/HomeCommon.vue
+++ panku-manage-front/src/components/HomeCommon.vue
@@ -19,36 +19,15 @@
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#ffd04b">
-            <el-submenu index="1">
+            <el-submenu v-for="(breadcrumb,indexs) in breadcrumbs" :index="indexs">
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>{{breadcrumb.breadcrumbName}}</span>
               </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item-group v-for="(subBreadcrumb,index) in breadcrumb.subBreadcrumb">
+                <el-menu-item @click="queryAllUser(subBreadcrumb.url)" :index="indexs+'-'+index">{{subBreadcrumb.breadcrumbName}}</el-menu-item>
               </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
             </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
           </el-menu>
         </el-col>
         </el-row>
@@ -71,23 +50,65 @@
 </template>
 
 <script>
+import { postAPI } from "../api/api";
+import {Message} from "element-ui";
+
 export default {
   name: 'HomeCommon',
+  created() {
+    this.getBreadcrumb()
+  },
+  watch: {
+    $route() {
+      this.getBreadcrumb()
+    }
+  },
   data () {
     return {
-      userData: {
-      }
+      userData: {},
+      breadcrumbs: {},
+      customers:{}
     }
   },
   mounted () {
     // this.userData = this.$route.params.userData
     this.userData = JSON.parse(localStorage.getItem("userInfo"))
   },
-  handleOpen(key, keyPath) {
-    console.log(key, keyPath);
-  },
-  handleClose(key, keyPath) {
-    console.log(key, keyPath);
+  methods:{
+    getBreadcrumb(){
+      let url = "api/common/queryBreadcrumb";
+      let query = {};
+      postAPI(url, query).then(response => {
+        console.info(">>>>>queryBreadcrumb>>>>>>" + JSON.stringify(response.data.data))
+        if (response.data.code == "0") {
+          this.breadcrumbs = response.data.data.breadcrumbs;
+        } else {
+          Message.error("获取菜单数据异常")
+        }
+      }).catch(err => {
+        Message.error("获取菜单数据异常")
+      });
+    },
+    queryAllUser(urls){
+      let url = urls;
+      let query = {};
+      postAPI(url, query).then(res => {
+        console.info(">>>>>query All user>>>>>>" + JSON.stringify(res.data.data))
+        if (res.data.code == "0") {
+          this.customers = res.data.data;
+        } else {
+          Message.error("获取用户数据异常")
+        }
+      }).catch(err => {
+        Message.error("获取用户数据异常")
+      });
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 }
 </script>
